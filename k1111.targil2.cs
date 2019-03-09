174e67f3a04ca78df38ff4794bbb324e4cfec549
bensimonlidor <47791397+bensimonlidor@users.noreply.github.com>
2019-03-09 18:39:59 +0200

    Create k1111.targil2.cs

---
+++ k1111.targil2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace div7
+{
+	class Program
+	{
+		static void Main(string[] args)
+        {
+          for (int i = 100; i >= 1; i--)
+			  {
+					Console.WriteLine(i);
+              }
+              
+       }
+
+		}
+	}
