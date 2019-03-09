c6bfc5c5c4638efabb565c8575fd10049a4b333f
bensimonlidor <47791397+bensimonlidor@users.noreply.github.com>
2019-03-10 01:17:22 +0200

    Create k1411.targil4.cs

---
+++ k1411.targil4.cs
@@ -0,0 +1,28 @@
+
+
+
+
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 7 ; i=i+2)
+            {
+                for (int j = 1; j <= i; j=j+1)
+                {
+
+                 Console.Write("*");
+   
+                }
+                  Console.WriteLine("    ");
+
+            }
+         
+            
+        }
+       
+        }
+}
