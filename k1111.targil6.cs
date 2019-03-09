1c57172699c00fdceb1abd5cc47cade034589c44
bensimonlidor <47791397+bensimonlidor@users.noreply.github.com>
2019-03-09 20:15:03 +0200

    Create k1111.targil6.cs

---
+++ k1111.targil6.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+          	for (int i = 7; i >= 1; i--)
+            {
+                	for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+               Console.WriteLine();
+ 
+          // with 4*
+          {
+          	for (int i = 4; i >= 1; i--)
+            {
+                	for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+               Console.WriteLine();
+                
+        }
+    }
+}
+}
+              
+        }
+    }
+}
+}
